b1dc6455f94d262cc3f0d30e302f0c5d480e11fb
nichel morales <usr3741@salesianos-lacuesta.net>
2026-04-21 13:19:09 +0100

    Aniado constructor y getter/setter

--- src/net/salesianos/restaurante/Restaurante.java
+++ src/net/salesianos/restaurante/Restaurante.java
@@ -9,4 +9,52 @@ public class Restaurante {
 
     public Restaurante() {
     }
+
+    public Restaurante(String nombre, String direccion, String tipoCocina, int puntuacion, String valoracion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.tipoCocina = tipoCocina;
+        this.puntuacion = puntuacion;
+        this.valoracion = valoracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTipoCocina() {
+        return tipoCocina;
+    }
+
+    public void setTipoCocina(String tipoCocina) {
+        this.tipoCocina = tipoCocina;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public String getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(String valoracion) {
+        this.valoracion = valoracion;
+    }
 }
