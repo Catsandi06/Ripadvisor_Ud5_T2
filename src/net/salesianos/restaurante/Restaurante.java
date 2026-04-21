6da355aa29748eb536e13a7113c739ddd760a09a
nichel morales <usr3741@salesianos-lacuesta.net>
2026-04-21 13:18:43 +0100

    Creo clase Restaurante con atributos

---
+++ src/net/salesianos/restaurante/Restaurante.java
@@ -0,0 +1,12 @@
+package net.salesianos;
+
+public class Restaurante {
+    private String nombre;
+    private String direccion;
+    private String tipoCocina;
+    private int puntuacion;
+    private String valoracion;
+
+    public Restaurante() {
+    }
+}
