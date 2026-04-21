1f205bfd010abfc5412ee57313d60063f87ba82a
nichel morales <usr3741@salesianos-lacuesta.net>
2026-04-21 13:23:47 +0100

    Llamo al metodo iniciar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import net.salesianos.InterfazRipAdvisor;
+
 public class App {
     public static void main(String[] args) {
         InterfazRipAdvisor interfaz = new InterfazRipAdvisor();
+        interfaz.iniciar();
     }
 }
