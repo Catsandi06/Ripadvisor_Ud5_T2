115b46d9cdc2962f4e9c2300c16139d23a0b9ca4
nichel morales <usr3741@salesianos-lacuesta.net>
2026-04-21 13:20:16 +0100

    Creo clase GestorRestaurantes

---
+++ src/net/salesianos/gestor/GestorRestaurantes.java
@@ -0,0 +1,12 @@
+package net.salesianos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorRestaurantes {
+    private List<Restaurante> restaurantes;
+
+    public GestorRestaurantes() {
+        restaurantes = new ArrayList<>();
+    }
+}
